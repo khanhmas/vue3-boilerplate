a0d332b8718836ed90c28be02cc7d50970563c27
alex <luquankhanh68@gmail.com>
2021-02-06 07:39:16 -0800

    config taildwind css

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,61 @@
+/* eslint-disable */
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
+
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
+    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier/@typescript-eslint'
   ],
+
+
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2021,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+    ecmaFeatures : {
+      jsx: false
+    }
   },
+
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    quotes: ['error', 'single', { avoidEscape: true }],
+    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 0,
+    'vue/html-indent': ['error', 4],
+    'vue/html-self-closing': 0,
+    'prefer-const': 0
   },
+
+  ignorePatterns: [
+    'node_modules/',
+    '*.js',
+    'shims-vue.d.ts'
+  ],
+
   overrides: [
     {
       files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    },
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
       ],
       env: {
         jest: true
